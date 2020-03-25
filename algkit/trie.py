64385e6ddb2fab3be00461ca9fd12613b433929b
ishiahirake <ishiahirake@gmail.com>
2020-03-25 23:17:22 +0900

    Add pattern search in the trie.

--- algkit/trie.py
+++ algkit/trie.py
@@ -1,13 +1,21 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
+        self._children = {}
         self.is_terminal = False
 
+    @property
+    def keys(self):
+        return self._children.keys()
+
+    @property
+    def children(self):
+        return self._children.values()
+
     def setdefault_child(self, char: str, default=None):
-        return self.children.setdefault(char, default if default else TrieNode())
+        return self._children.setdefault(char, default if default else TrieNode())
 
     def get_child(self, char: str):
-        return self.children.get(char)
+        return self._children.get(char)
 
 
 class Trie:
@@ -48,3 +56,25 @@ class Trie:
             if node is None:
                 return None
         return node
+
+    def pattern_search(self, pattern: str) -> bool:
+        """
+        Returns if the word is in this trie.
+        A word could contain the dot character '.' to represent any one letter.
+        """
+        return self._search_by_pattern(self.node, pattern)
+
+    def _search_by_pattern(self, node, pattern: str) -> bool:
+        if not node:
+            return False
+        if not pattern:
+            return node.is_terminal
+
+        char, left = pattern[0], pattern[1:]
+        if char != '.':
+            return self._search_by_pattern(node.get_child(char), left)
+        else:
+            for child in node.children:
+                if self._search_by_pattern(child, left):
+                    return True
+            return False
--- tests/trie.test.py
+++ tests/trie.test.py
@@ -18,6 +18,17 @@ class TrieTestCase(unittest.TestCase):
         trie.insert('Apple')
         self.assertTrue(trie.starts_with('App'))
 
+    def test_pattern_search(self):
+        wd = Trie()
+        wd.insert("bad")
+        wd.insert("dad")
+        wd.insert("mad")
+
+        self.assertFalse(wd.pattern_search("pad"))
+        self.assertTrue(wd.pattern_search("bad"))
+        self.assertTrue(wd.pattern_search(".ad"))
+        self.assertTrue(wd.pattern_search("b.."))
+
 
 if __name__ == '__main__':
     unittest.main()
